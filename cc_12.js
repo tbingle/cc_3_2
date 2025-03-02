0d3e95a9eb50f87b8e7ce621a7060360c1c0d39a
tbingle <sandmanthomas9@icloud.com>
2025-03-02 14:37:25 -0500

    Task 1- Created Revenue Metric Card

--- cc_12.js
+++ cc_12.js
@@ -1,1 +1,14 @@
 //Inital Commit
+//Task 1
+
+const dashboardById = document.getElementById("dashboard"); //Using getElementById
+const dashboardByQuery = document.querySelector("#dashboard"); //Using querySelector
+const revenueCard = document.createElement("div");//Using createElement
+revenueCard.setAttribute("class", "metric-card");
+revenueCard.setAttribute("id", "revenueCard");
+revenueCard.innerHTML = `
+  <h3>Revenue</h3>
+  <p>$0</p>
+`;
+dashboardById.appendChild(revenueCard); // Or you can use dashboardByQuery.appendChild(revenueCard);
+
