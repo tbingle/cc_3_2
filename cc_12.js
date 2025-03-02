e1cfb460c712440566dcbc3e365100c44dc3e8ac
tbingle <sandmanthomas9@icloud.com>
2025-03-02 14:42:20 -0500

    Task 2- Updated Metric Cards via Array Conversion

--- cc_12.js
+++ cc_12.js
@@ -12,3 +12,14 @@ revenueCard.innerHTML = `
 `;
 dashboardById.appendChild(revenueCard); // Or you can use dashboardByQuery.appendChild(revenueCard);
 
+//Task 2
+const metricCardsNodeList = document.querySelectorAll(".metric-card");//Select Elements
+
+const metricCardsArray = Array.from(metricCardsNodeList);//Convert to list
+
+metricCardsArray.forEach(card => {//Update information
+ 
+  const title = card.querySelector("h3");
+  title.innerText += " - Updated"; 
+  card.style.backgroundColor = "#f3f3f3"; 
+});
