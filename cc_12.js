06d5c7c67fdef8d922fc51762b4dfce9790de9e7
tbingle <sandmanthomas9@icloud.com>
2025-03-02 14:49:55 -0500

    Task 3- Implemented Dynamic Inventory List

--- cc_12.js
+++ cc_12.js
@@ -13,13 +13,41 @@ revenueCard.innerHTML = `
 dashboardById.appendChild(revenueCard); // Or you can use dashboardByQuery.appendChild(revenueCard);
 
 //Task 2
+
 const metricCardsNodeList = document.querySelectorAll(".metric-card");//Select Elements
-
 const metricCardsArray = Array.from(metricCardsNodeList);//Convert to list
-
 metricCardsArray.forEach(card => {//Update information
- 
   const title = card.querySelector("h3");
   title.innerText += " - Updated"; 
   card.style.backgroundColor = "#f3f3f3"; 
 });
+
+//Task 3
+
+const inventoryList = document.getElementById("inventoryList");
+//Function to add a new inventory item to the list
+function addInventoryItem(productName) {
+    //Create a new element
+    const newProduct = document.createElement("li");
+    //Set a class 
+    newProduct.setAttribute("class", "product-item");
+    newProduct.setAttribute("data-name", productName); //Adding the product name 
+    newProduct.innerText = productName;
+    //New product item 
+    inventoryList.appendChild(newProduct);
+    //Remove the product item on click
+    newProduct.addEventListener("click", removeInventoryItem);
+}
+//Remove a product item when clicked
+function removeInventoryItem(event) {
+    //Remove the clicked item
+    inventoryList.removeChild(event.target);
+}
+function createCustomerCard(name) {
+    const customerCard = document.createElement('div');
+    customerCard.classList.add('customer-card');
+    customerCard.innerText = `Customer: ${name}`;
+    document.getElementById('customerSection').appendChild(customerCard);
+}
+
+
